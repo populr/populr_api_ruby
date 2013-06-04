e1884648e1d9efdfdbf7f4e3a9b75654fc382011
Ben Gotow <bengotow@gmail.com>
2013-06-04 17:16:05 -0500

    Adding more attributes to templates, which have existed but haven't been in use

--- lib/template.rb
+++ lib/template.rb
@@ -4,6 +4,9 @@ class Template < RestfulModel
 
   attr_accessor :title
   attr_accessor :name
+  attr_accessor :domain
+  attr_accessor :password
+  attr_accessor :background_image_asset_id
   attr_accessor :label_names
   attr_accessor :api_tags
   attr_accessor :api_regions
